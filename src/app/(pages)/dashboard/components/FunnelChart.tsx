dd9d198e713bb2fa11a87907befd38371fabb33d
Marsad <marsad11223@gmail.com>
2024-09-05 05:24:55 +0500

    integrate Funnel charts

--- src/app/(pages)/dashboard/components/FunnelChart.tsx
+++ src/app/(pages)/dashboard/components/FunnelChart.tsx
@@ -1,92 +1,27 @@
 "use client";
 
-import { Box } from "@mui/material";
-import dynamic from "next/dynamic";
+// react imports
 import React from "react";
+import dynamic from "next/dynamic";
+
+// third party imports
+import { Box } from "@mui/material";
 import { ApexOptions } from "apexcharts";
 
+// local imports
+import { getFunnelOptions, processFunnelData } from "@/app/_utils/helpers";
+
+// Dynamically import the Chart component
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const FunnelChart: React.FC<{ data: ConversionFunnelResponse }> = ({
   data,
 }) => {
-  // Define categories and values
-  const categories = ["Searches", "Views", "Clicks", "Time Spent"];
-  const values = [data.searches, data.views, data.clicks, data.totalTimeSpent];
-
-  // Combine categories and values into a sortable array of objects
-  const dataPairs = categories.map((category, index) => ({
-    category,
-    value: values[index],
-  }));
-
-  // Sort the data pairs in descending order based on value
-  dataPairs.sort((a, b) => b.value - a.value);
-
-  // Extract sorted categories and values
-  const sortedCategories = dataPairs.map((pair) => pair.category);
-  const sortedValues = dataPairs.map((pair) => pair.value);
-
-  // Chart options
-  const options: ApexOptions = {
-    chart: {
-      type: "bar",
-      height: 350,
-    },
-    plotOptions: {
-      bar: {
-        borderRadius: 4,
-        horizontal: true,
-        barHeight: "80%",
-        isFunnel: true,
-      },
-    },
-    dataLabels: {
-      enabled: true,
-      formatter: function (val: number, opt: any) {
-        return `${opt.w.globals.labels[opt.dataPointIndex]}: ${val}`;
-      },
-      dropShadow: {
-        enabled: true,
-        top: 2,
-        left: 2,
-        blur: 4,
-        opacity: 0.5,
-      },
-      style: {
-        fontSize: "12px",
-        fontWeight: "bold",
-        colors: ["#333"],
-      },
-    },
-    title: {
-      text: "Recruitment Funnel",
-      align: "center", // Corrected to match ApexOptions type
-      style: {
-        fontSize: "20px",
-        fontWeight: "bold",
-        color: "#333",
-      },
-    },
-    xaxis: {
-      categories: sortedCategories,
-      labels: {
-        style: {
-          fontSize: "12px",
-          colors: ["#333"],
-        },
-      },
-    },
-    legend: {
-      show: false,
-    },
-    colors: ["#008FFB"],
-  };
-
-  // Series data for the chart
+  const { sortedCategories, sortedValues } = processFunnelData(data);
+  const options: ApexOptions = getFunnelOptions(sortedCategories, sortedValues);
   const series = [
     {
-      name: "Funnel Series",
+      name: "Funnel visualization",
       data: sortedValues,
     },
   ];
--- src/app/_utils/helpers.ts
+++ src/app/_utils/helpers.ts
@@ -1,17 +1,17 @@
-// utils/sessionUtils.ts
-
 import { v4 as uuidv4 } from "uuid";
+import { ApexOptions } from "apexcharts";
 
+// session helpers
 export const getSessionId = (): string => {
   if (typeof window !== "undefined") {
     let sessionId = localStorage.getItem("sessionId");
     if (!sessionId) {
-      sessionId = uuidv4(); // Generate a new UUID
+      sessionId = uuidv4();
       localStorage.setItem("sessionId", sessionId);
     }
     return sessionId;
   }
-  return uuidv4(); // Return a new UUID if not in the browser environment
+  return uuidv4();
 };
 
 export const setSessionId = (sessionId: string): void => {
@@ -24,4 +24,88 @@ export const clearSessionId = (): void => {
   if (typeof window !== "undefined") {
     localStorage.removeItem("sessionId");
   }
+};
+
+// funnel chart helpers
+export const getFunnelOptions = (
+  categories: string[],
+  values: number[]
+): ApexOptions => {
+  return {
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+        barHeight: "80%",
+        isFunnel: true,
+      },
+    },
+    dataLabels: {
+      enabled: true,
+      formatter: function (val: number, opt: any) {
+        return `${opt.w.globals.labels[opt.dataPointIndex]}: ${val}`;
+      },
+      dropShadow: {
+        enabled: true,
+        top: 2,
+        left: 2,
+        blur: 4,
+        opacity: 0.5,
+      },
+      style: {
+        fontSize: "12px",
+        fontWeight: "bold",
+        colors: ["#333"],
+      },
+    },
+    title: {
+      text: "Funnel visualization",
+      align: "center",
+      style: {
+        fontSize: "20px",
+        fontWeight: "bold",
+        color: "#333",
+      },
+    },
+    xaxis: {
+      categories: categories,
+      labels: {
+        style: {
+          fontSize: "12px",
+          colors: ["#333"],
+        },
+      },
+    },
+    legend: {
+      show: false,
+    },
+    colors: ["#008FFB"],
+  };
+};
+
+export const processFunnelData = (
+  data: ConversionFunnelResponse
+): {
+  sortedCategories: string[];
+  sortedValues: number[];
+} => {
+  const categories = ["Searches", "Views", "Clicks", "Time Spent"];
+  const values = [data.searches, data.views, data.clicks, data.totalTimeSpent];
+
+  // Combine and sort data
+  const dataPairs = categories
+    .map((category, index) => ({
+      category,
+      value: values[index],
+    }))
+    .sort((a, b) => b.value - a.value);
+
+  return {
+    sortedCategories: dataPairs.map((pair) => pair.category),
+    sortedValues: dataPairs.map((pair) => pair.value),
+  };
 };
